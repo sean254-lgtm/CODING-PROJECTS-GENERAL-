c8a437b5b0dce6ecf0da9feabc7527ed8ef16427
Sean Kiprop <seanp1521@gmail.com>
2026-07-21 19:57:52 +0300

    Add profile lookup functionality with contacts
    This file contains a contact list and a function to look up profile information based on the name and property.

---
+++ FULL STACK DEVELOPMENT/FRONT-END DEVELOPMENT/JAVASCRIPT/profile lookup.js
@@ -0,0 +1,44 @@
+let contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop){
+  for(let i = 0; i < contacts.length; i++){
+    if(contacts[i].firstName === name){
+      if(!contacts[i].hasOwnProperty(prop)){
+        return "No such property";
+      } else {
+        return contacts[i][prop];
+      }
+    }
+  }
+  return "No such contact";
+}
+
+console.log(lookUpProfile("Akira", "lastName"));
+console.log(lookUpProfile("Harry", "likes"));
+console.log(lookUpProfile("Sherlock", "number"));
+
